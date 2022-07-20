5b986c02267ddbe58b8555a31be06db07b0211db
apple <apple@apples-MacBook-Pro.local>
2022-07-20 16:56:37 +0300

    first commit

---
+++ restaurants/models.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from django.db import models
+
+# Create your models here.
+
+class Restaurant(models.Model):
+
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100,default="")
+    opening_time = models.TimeField(default="00:00:00")
+    closing_time= models.TimeField(default="00:00:00")
+    created_at = models.DateField(auto_now_add=True)
+    
+
+
